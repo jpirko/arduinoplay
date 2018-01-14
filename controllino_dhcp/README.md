de654c58d39c5ddfa59cd51a2474aee46e1661a5
Jiri Pirko <jiri@resnulli.us>
2018-01-14 12:06:40 +0100

    add controllino dhcp hello world
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

---
+++ controllino_dhcp/README.md
@@ -0,0 +1,24 @@
+# DHCP hello world for Controllino
+
+## Install
+
+As a dependency, you have to have PlatformIO installed. Please see [PlatformIO installation] documentation.
+
+```
+$ pio lib install "Controllino"
+$ mkdir lib
+$ cd lib
+$ git clone https://github.com/jpirko/Ethernet.git
+$ cd ..
+$ platformio run --target upload
+$ platformio device monitor
+```
+Note that the forked Ethernet library is needed in other for Ethernet to work. the reason is Controllino uses different SPI CS pin comparing to the original Arduino UNO Ethernet shield. At the time you read this, this might be resolved already by upstream Ethernet library.
+
+## Parts List
+
+* [Controllino]
+
+[PlatformIO installation]: http://docs.platformio.org/en/latest/installation.html
+[Controllino]: https://controllino.biz/
+
---
+++ controllino_dhcp/src/main.cpp
@@ -0,0 +1,76 @@
+/*
+ * Controllino: DHCP hello world
+ * Copyright (c) 2017 Jiri Pirko <jiri@resnulli.us>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "Arduino.h"
+#include <Controllino.h>
+#include <SPI.h>
+#include <Ethernet.h>
+
+EthernetClient client;
+
+void print_address()
+{
+	int i;
+
+	Serial.print("IP address: ");
+	for (i = 0; i < 4; i++) {
+		if (i)
+			Serial.print(".");
+		Serial.print(Ethernet.localIP()[i], DEC);
+	}
+	Serial.println();
+}
+
+byte mac[] = {
+	0x00, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE
+};
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("Controllino DHCP hello world");
+
+	if (Ethernet.begin(mac) == 0) {
+		Serial.println("Failed to get IP address using DHCP");
+		return;
+	}
+	print_address();
+}
+
+void loop() {
+	switch (Ethernet.maintain()) {
+	case 1:
+		Serial.println("Renew failed");
+		break;
+	case 2:
+		Serial.println("Renew success");
+		print_address();
+		break;
+	case 3:
+		Serial.println("Rebind failed");
+		break;
+	case 4:
+		Serial.println("Rebind success");
+		print_address();
+		break;
+	default:
+		break;
+
+	}
+}
+
