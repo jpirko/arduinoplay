69d1156e6d6835f2bb0aac29a27e049581df90c9
Jiri Pirko <jiri@resnulli.us>
2019-04-30 09:05:32 +0200

    add Arduino Nano blink hello world
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

---
+++ nano_blink/README.md
@@ -0,0 +1,18 @@
+# Blink hello world for Arduino Nano v3 ATMEGA328P
+
+## Install
+
+As a dependency, you have to have PlatformIO installed. Please see [PlatformIO installation] documentation.
+
+```
+$ platformio run --target upload
+$ platformio device monitor
+```
+
+## Parts List
+
+* [Arduino Nano v3 with ATMEGA328P]
+
+[PlatformIO installation]: http://docs.platformio.org/en/latest/installation.html
+[Arduino Nano v3 with ATMEGA328P]: https://www.aliexpress.com/snapshot/0.html?spm=a2g0s.9042647.0.0.57f04c4deOqsZx&orderId=100848359295850&productId=32729710918
+
---
+++ nano_blink/src/main.cpp
@@ -0,0 +1,35 @@
+/*
+ * Arduino Nano: led blink hello world
+ * Copyright (c) 2019 Jiri Pirko <jiri@resnulli.us>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include "Arduino.h"
+
+#define LED_BUILTIN 1
+
+void setup()
+{
+	pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+	digitalWrite(LED_BUILTIN, HIGH);
+	delay(100);
+	digitalWrite(LED_BUILTIN, LOW);
+	delay(100);
+}
