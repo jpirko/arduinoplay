b59e466e4314bc8568b9100e66604056aaea947e
Jiri Pirko <jiri@resnulli.us>
2021-11-29 06:54:24 +0100

    controllino_mqqt_io: remove OneWire Dallas temperatures code
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

--- controllino_mqtt_io/src/main.cpp
+++ controllino_mqtt_io/src/main.cpp
@@ -22,17 +22,11 @@
 #include <SPI.h>
 #include <Ethernet.h>
 #include <PubSubClient.h>
-#include <OneWire.h>
 #include <DallasTemperature.h>
 #include <EEPROM.h>
 
 EthernetClient ethClient;
 PubSubClient client(ethClient);
-
-#define ONE_WIRE_BUS 20
-
-OneWire oneWire(ONE_WIRE_BUS);
-DallasTemperature sensors(&oneWire);
 
 #define NAME_SIZE 20
 char name[NAME_SIZE];
@@ -495,137 +489,6 @@ void pins_init(void)
 	}
 }
 
-static void ow_temp_init(void)
-{
-	sensors.begin();
-	sensors.setWaitForConversion(false);
-}
-
-struct ow_temp_device {
-	DeviceAddress address;
-};
-
-#define OW_TEMP_DEVICE_MAX 16
-
-struct ow_temp {
-	bool read_in_progress;
-	unsigned long last_read_millis;
-	bool waiting_on_conversion;
-	unsigned long start_conversion_millis;
-	bool had_devices;
-	int current_device;
-	uint8_t device_count;
-	struct ow_temp_device device[OW_TEMP_DEVICE_MAX];
-};
-
-static void ow_temp_scan(struct ow_temp *temp, unsigned int interval)
-{
-	unsigned long now = millis();
-	DeviceAddress address;
-
-	if (temp->last_read_millis &&
-	    (now - temp->last_read_millis < interval))
-		return;
-
-	memset(temp, 0, sizeof(*temp));
-	oneWire.reset_search();
-	while (oneWire.search(address)) {
-		if (OneWire::crc8(address, 7) != address[7] ||
-		    temp->device_count == OW_TEMP_DEVICE_MAX)
-			continue;
-		memcpy(temp->device[temp->device_count++].address, address,
-		       sizeof(address));
-	}
-	if (temp->device_count) {
-		temp->read_in_progress = true;
-		temp->had_devices = true;
-	} else if (temp->had_devices) {
-		ow_temp_init();
-	}
-}
-
-static bool ow_temp_device_next(struct ow_temp *temp)
-{
-	unsigned long now = millis();
-
-	temp->current_device++;
-	if (temp->device_count == temp->current_device) {
-		temp->read_in_progress = false;
-		temp->last_read_millis = now;
-		return false;
-	}
-	return true;
-}
-
-static void ow_temp_conversion_start(struct ow_temp *temp)
-{
-	while (!sensors.requestTemperaturesByAddress(temp->device[temp->current_device].address)) {
-		if (!ow_temp_device_next(temp))
-			return;
-	}
-	temp->waiting_on_conversion = true;
-	temp->start_conversion_millis = millis();;
-}
-
-char *ow_temp_topic(DeviceAddress deviceAddress)
-{
-	int offset = 0;
-	int i;
-
-	offset = snprintf(tmp_buf, TMP_BUF_LEN, "%s/temp/", name);
-	for (i = 0; i < 8; i++)
-		offset += snprintf(tmp_buf + offset, TMP_BUF_LEN - offset,
-				   "%02x", deviceAddress[i]);
-	Serial.println(tmp_buf);
-	return tmp_buf;
-}
-
-void ow_temp_publish(DeviceAddress deviceAddress, float temp_value)
-{
-	char temp_buf[16];
-
-//	sprintf(temp_buf, "%f", temp_value);
-	dtostrf(temp_value, 4, 2, temp_buf);
-	client.publish(ow_temp_topic(deviceAddress), temp_buf);
-}
-
-#define OW_TEMP_CONVERSION_TIMEOUT 950
-
-static void ow_temp_conversion_wait_check(struct ow_temp *temp)
-{
-	unsigned long now = millis();
-	float temp_value;
-
-	if (!sensors.isConversionComplete()) {
-		if (now - temp->start_conversion_millis > OW_TEMP_CONVERSION_TIMEOUT)
-			goto next;
-		return;
-	}
-
-	temp_value = sensors.getTempC(temp->device[temp->current_device].address);
-	if (temp_value != DEVICE_DISCONNECTED_C)
-		ow_temp_publish(temp->device[temp->current_device].address,
-				temp_value);
-
-next:
-	temp->waiting_on_conversion = false;
-	ow_temp_device_next(temp);
-}
-
-static void ow_temp_process(struct ow_temp *temp, unsigned int interval)
-{
-	if (!temp->read_in_progress)
-		ow_temp_scan(temp, interval);
-	else {
-		if (!temp->waiting_on_conversion)
-			ow_temp_conversion_start(temp);
-		else
-			ow_temp_conversion_wait_check(temp);
-	}
-}
-
-static struct ow_temp ow_temp;
-
 void print_ip(IPAddress ip)
 {
 	int i;
@@ -819,7 +682,6 @@ void setup(void)
 
 	Ethernet.begin(mac, ip);
 	pins_init();
-	ow_temp_init();
 
 	client.setServer(mqttip, 1883);
 	client.setCallback(callback);
@@ -864,7 +726,6 @@ void loop(void)
 	} else {
 		input_pins_update_state();
 		input_pins_publish(true);
-		ow_temp_process(&ow_temp, temp_interval);
 		client.loop();
 	}
 	pins_emerg_off_timeout_check(now, emerg_off_timeout);
