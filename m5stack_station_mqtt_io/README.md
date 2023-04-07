43eb0c7ca2d3e7a9d2b2386702ea3baa26a5c6a4
Jiri Pirko <jiri@resnulli.us>
2023-04-07 15:10:50 +0200

    add M5Stack Station MQTT IO client
    Signed-off-by: Jiri Pirko <jiri@resnulli.us>

---
+++ m5stack_station_mqtt_io/README.md
@@ -0,0 +1,116 @@
+# M5Stack Station MQTT IO client
+
+## Install
+
+As a dependency, you have to have PlatformIO installed. Please see [PlatformIO installation] documentation.
+
+```
+$ platformio run --target upload
+$ platformio device monitor --baud 115200
+```
+
+On the console, you should see this:
+```
+OK
+INIT
+NAME:test
+SSID:testssid
+PASS:12345678
+MAC:aabbccddeeff
+IP:172.22.1.10
+MQTTIP:172.22.1.1
+FILTER:16
+THRESHOLD:16
+PIN FLAVOURS:
+G32: disabled
+G33: disabled
+G25: disabled
+G35: disabled
+G26: disabled
+G36: disabled
+G14: disabled
+G13: disabled
+G17: disabled
+G16: disabled
+INIT DONE
+WIFI CONNECTED
+MQTT CONNECTED
+```
+
+These are configuration defaults.
+
+## Usage
+
+MQTT broker port is const: 1883.
+
+The name of the device is the MAC address without ":". The name
+is used in MQTT topics. For example:
+
+```
+test/filter
+```
+
+## Configuration
+
+The device is configurable over MQTT, the topics it subscribes to are:
+```
+[NAME]/config/mac (device MAC address)
+[NAME]/config/ip (device IP address)
+[NAME]/config/mqttip (MQTT broker IP address)
+[NAME]/config/filter (digital input filter - max 32)
+[NAME]/config/threshold (analog input threshold - max 255)
+[NAME]/config/G26 (pin flavour, one of "dout", "pwmout", "din", "ain")
+..
+[NAME]/config/G16 (pin flavour, one of "dout", "pwmout", "din", "ain")
+```
+
+## Output control
+
+Digital and PWM outputs could be controlled over MQTT messages.
+The topics device subscribes to are:
+
+```
+[NAME]/dout/G32
+..
+[NAME]/dout/G16
+[NAME]/pwmout/G32
+..
+[NAME]/pwmout/G16
+```
+
+## Input monitoring
+
+Device publishes following topics with digital and analog input values:
+
+```
+[NAME]/din/G32
+..
+[NAME]/din/G16
+[NAME]/ain/G32
+..
+[NAME]/ain/G16
+```
+
+## Examples
+
+Execute following batch of commands with desired configuration values:
+
+```
+mosquitto_pub -h 172.22.1.1 -t test/config/mac -m aa:22:33:44:55:66
+mosquitto_pub -h 172.22.1.1 -t test/config/ssid -m somenetwork
+mosquitto_pub -h 172.22.1.1 -t test/config/pass -m secretpass
+mosquitto_pub -h 172.22.1.1 -t test/config/ip -m 192.168.1.50
+mosquitto_pub -h 172.22.1.1 -t test/config/mqttip -m 192.168.1.1
+mosquitto_pub -h 172.22.1.1 -t test/config/filter -m 30
+mosquitto_pub -h 172.22.1.1 -t test/config/threshold -m 20
+mosquitto_pub -h 172.22.1.1 -t test/config/G32 -m ain
+mosquitto_pub -h 172.22.1.1 -t test/config/G33 -m dout
+mosquitto_pub -h 172.22.1.1 -t test/config/G14 -m pwmout
+```
+
+Please note that "172.22.1.1" is the address of MQTT broker. Make sure you set
+new addresses of the device (ip) and the MQTT broker (mqttip) correctly.
+
+```
+
+[PlatformIO installation]: http://docs.platformio.org/en/latest/installation.html
---
+++ m5stack_station_mqtt_io/src/main.cpp
@@ -0,0 +1,604 @@
+/*
+ * M5Stack Station: MQTT I/O client
+ * Copyright (c) 2023 Jiri Pirko <jiri@resnulli.us>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#include <M5Station.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <EEPROM.h>
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+#define NAME_SIZE 20
+char name[NAME_SIZE];
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof((x)[0]))
+
+enum pin_type {
+	PIN_TYPE_G,
+};
+
+enum pin_flavour {
+	PIN_FLAVOUR_DISABLED,
+	PIN_FLAVOUR_DIGITAL_INPUT,
+	PIN_FLAVOUR_ANALOG_INPUT,
+	PIN_FLAVOUR_DIGITAL_OUTPUT,
+	PIN_FLAVOUR_PWM_OUTPUT,
+};
+
+const char *pin_type_subtopic[] = {
+	[PIN_TYPE_G] = "G",
+};
+
+const char *pin_flavour_subtopic[] = {
+	[PIN_FLAVOUR_DISABLED] = "disabled",
+	[PIN_FLAVOUR_DIGITAL_INPUT] = "din",
+	[PIN_FLAVOUR_ANALOG_INPUT] = "ain",
+	[PIN_FLAVOUR_DIGITAL_OUTPUT] = "dout",
+	[PIN_FLAVOUR_PWM_OUTPUT] = "pwmout",
+};
+
+struct pin {
+	uint8_t type;
+	uint8_t index; /* index within the group */
+	uint8_t pin; /* pin number */
+	uint16_t old_state;
+	uint16_t state;
+	uint8_t new_state_cnt;
+	uint8_t flavour;
+};
+
+bool is_pin_output(struct pin *pin)
+{
+	return pin->flavour == PIN_FLAVOUR_DIGITAL_OUTPUT ||
+	       pin->flavour == PIN_FLAVOUR_PWM_OUTPUT;
+}
+
+#define PIN_G(_index, _pin) {								\
+	.type = PIN_TYPE_G,								\
+	.index = _index,								\
+	.pin = _pin,									\
+}
+
+struct pin pins[] = {
+	PIN_G(32, 32), /* G32 */
+	PIN_G(33, 33), /* G33 */
+	PIN_G(25, 25), /* G25 */
+	PIN_G(35, 35), /* G35 */
+	PIN_G(26, 26), /* G26 */
+	PIN_G(36, 36), /* G36 */
+	PIN_G(14, 14), /* G14 */
+	PIN_G(13, 13), /* G13 */
+	PIN_G(17, 17), /* G17 */
+	PIN_G(16, 16), /* G16 */
+};
+
+#define PINS_COUNT ARRAY_SIZE(pins)
+
+struct pin_flavours {
+	uint8_t pin_flavours[PINS_COUNT];
+};
+
+#define for_each_pin(pin, i)								\
+	for (i = 0, pin = &pins[i]; i < PINS_COUNT; pin = &pins[++i])
+
+#define TMP_BUF_LEN 64
+char tmp_buf[TMP_BUF_LEN];
+
+char *pin_topic(struct pin *pin)
+{
+	snprintf(tmp_buf, TMP_BUF_LEN, "%s/%s/%s%u", name,
+		 pin_flavour_subtopic[pin->flavour],
+		 pin_type_subtopic[pin->type], pin->index);
+	return tmp_buf;
+}
+
+char *pin_config_topic(struct pin *pin)
+{
+	snprintf(tmp_buf, TMP_BUF_LEN, "%s/config/%s%u", name,
+		 pin_type_subtopic[pin->type], pin->index);
+	return tmp_buf;
+}
+
+struct pin_flavours pin_flavours;
+
+void load_print_pin_flavours(void)
+{
+	struct pin *pin;
+	uint8_t i;
+
+	for_each_pin(pin, i) {
+		Serial.print(pin_type_subtopic[pin->type]);
+		Serial.print(pin->index);
+		Serial.print(": ");
+		pin->flavour = pin_flavours.pin_flavours[i];
+		Serial.println(pin_flavour_subtopic[pin->flavour]);
+	}
+}
+
+void pin_publish(struct pin *pin)
+{
+	char state_buf[16];
+
+	sprintf(state_buf, "%u", pin->state);
+	client.publish(pin_topic(pin), state_buf, true);
+}
+
+void input_pins_update_state()
+{
+	uint16_t new_state;
+	struct pin *pin;
+	uint8_t i;
+
+	for_each_pin(pin, i) {
+		switch (pin->flavour) {
+		case PIN_FLAVOUR_DIGITAL_INPUT:
+			new_state = digitalRead(pin->pin);
+			break;
+		case PIN_FLAVOUR_ANALOG_INPUT:
+			new_state = analogRead(pin->pin);
+			break;
+		default:
+			continue;
+		}
+		pin->state = new_state;
+	}
+}
+
+uint8_t input_filter;
+uint8_t input_threshold;
+
+void input_pins_publish(bool changed_only)
+{
+	unsigned int delta;
+	struct pin *pin;
+	uint8_t i;
+
+	for_each_pin(pin, i) {
+		switch (pin->flavour) {
+		case PIN_FLAVOUR_DIGITAL_INPUT:
+			if (changed_only) {
+				if (pin->old_state == pin->state) {
+					pin->new_state_cnt = 0;
+					continue;
+				}
+				if (pin->new_state_cnt++ < input_filter)
+					continue;
+			}
+			pin_publish(pin);
+			pin->old_state = pin->state;
+			pin->new_state_cnt = 0;
+			break;
+		case PIN_FLAVOUR_ANALOG_INPUT:
+			delta = abs((int) pin->old_state - (int) pin->state);
+			if (delta < input_threshold && changed_only)
+				continue;
+			pin_publish(pin);
+			pin->old_state = pin->state;
+			pin->new_state_cnt = 0;
+			break;
+		default:
+			continue;
+		}
+	}
+}
+
+void output_pin_update_state(struct pin *pin, uint8_t new_state)
+{
+	pin->state = new_state;
+	switch (pin->flavour) {
+	case PIN_FLAVOUR_DIGITAL_OUTPUT:
+		digitalWrite(pin->pin, pin->state);
+		break;
+	case PIN_FLAVOUR_PWM_OUTPUT:
+		analogWrite(pin->pin, pin->state);
+		break;
+	}
+}
+
+void pins_msg_process(const char *topic, const char *value)
+{
+	struct pin *pin;
+	unsigned int i;
+
+	for_each_pin(pin, i) {
+		if (is_pin_output(pin) && !strcmp(topic, pin_topic(pin)))
+			output_pin_update_state(pin, strtol(value, NULL, 10));
+	}
+}
+
+void pins_subscribe(void)
+{
+	struct pin *pin;
+	unsigned int i;
+
+	for_each_pin(pin, i) {
+		client.subscribe(pin_config_topic(pin));
+		if (is_pin_output(pin))
+			client.subscribe(pin_topic(pin));
+	}
+}
+
+void pins_init(void)
+{
+	struct pin *pin;
+	uint8_t i;
+
+	for_each_pin(pin, i) {
+		switch (pin->flavour){
+		case PIN_FLAVOUR_DIGITAL_INPUT:
+			pinMode(pin->pin, INPUT_PULLUP);
+			break;
+		case PIN_FLAVOUR_ANALOG_INPUT:
+			pinMode(pin->pin, INPUT);
+			break;
+		case PIN_FLAVOUR_DIGITAL_OUTPUT: /* fall-through */
+		case PIN_FLAVOUR_PWM_OUTPUT:
+			pinMode(pin->pin, OUTPUT);
+			digitalWrite(pin->pin, 0);
+			break;
+		}
+	}
+}
+
+char *config_topic(const char *item)
+{
+	snprintf(tmp_buf, TMP_BUF_LEN, "%s/config/%s", name, item);
+	return tmp_buf;
+}
+
+void print_ip(IPAddress ip)
+{
+	int i;
+
+	for (i = 0; i < 4; i++) {
+		if (i)
+			Serial.print(".");
+		Serial.print(ip[i], DEC);
+	}
+	Serial.println();
+}
+
+uint32_t eeprom_magic = 0x6a3b1fac;
+char eeprom_default_name[] = "test";
+char eeprom_default_wifi_ssid[] = "testssid";
+char eeprom_default_wifi_pass[] = "12345678";
+byte eeprom_default_mac[] = {0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff};
+IPAddress eeprom_default_ip = IPAddress(172, 22, 1, 10);
+IPAddress eeprom_default_mqttip = IPAddress(172, 22, 1, 1);
+uint8_t eeprom_default_filter = 16;
+uint8_t eeprom_default_threshold = 16;
+#define EEPROM_FILTER_MAX 32
+#define EEPROM_THRESHOLD_MAX 255
+static struct pin_flavours eeprom_default_pin_flavours; /* all zeroes means all are disabled */
+
+#define EEPROM_MAGIC_OFFSET 0
+#define EEPROM_MAGIC_SIZE sizeof(eeprom_magic)
+
+#define WIFI_SSID_LEN 32
+#define WIFI_PASS_LEN 32
+#define MAC_LEN sizeof(eeprom_default_mac)
+#define IP_LEN sizeof(IPAddress)
+
+#define EEPROM_NAME_OFFSET EEPROM_MAGIC_OFFSET + EEPROM_MAGIC_SIZE
+#define EEPROM_NAME_SIZE NAME_SIZE
+
+#define EEPROM_WIFI_SSID_OFFSET EEPROM_NAME_OFFSET + EEPROM_NAME_SIZE
+#define EEPROM_WIFI_SSID_SIZE WIFI_SSID_LEN
+
+#define EEPROM_WIFI_PASS_OFFSET EEPROM_WIFI_SSID_OFFSET + EEPROM_WIFI_SSID_SIZE
+#define EEPROM_WIFI_PASS_SIZE WIFI_PASS_LEN
+
+#define EEPROM_MAC_OFFSET EEPROM_WIFI_PASS_OFFSET + EEPROM_WIFI_PASS_SIZE
+#define EEPROM_MAC_SIZE MAC_LEN
+
+#define EEPROM_IP_OFFSET EEPROM_MAC_OFFSET + EEPROM_MAC_SIZE
+#define EEPROM_IP_SIZE IP_LEN
+
+#define EEPROM_MQTTIP_OFFSET EEPROM_IP_OFFSET + EEPROM_IP_SIZE
+#define EEPROM_MQTTIP_SIZE IP_LEN
+
+#define EEPROM_FILTER_OFFSET EEPROM_MQTTIP_OFFSET + EEPROM_MQTTIP_SIZE
+#define EEPROM_FILTER_SIZE sizeof(eeprom_default_filter)
+
+#define EEPROM_THRESHOLD_OFFSET EEPROM_FILTER_OFFSET + EEPROM_FILTER_SIZE
+#define EEPROM_THRESHOLD_SIZE sizeof(eeprom_default_threshold)
+
+#define EEPROM_PIN_FLAVOURS_OFFSET EEPROM_THRESHOLD_OFFSET + EEPROM_THRESHOLD_SIZE
+#define EEPROM_PIN_FLAVOURS_SIZE sizeof(eeprom_default_pin_flavours)
+
+#define EEPROM_SIZE EEPROM_PIN_FLAVOURS_OFFSET + EEPROM_PIN_FLAVOURS_SIZE
+
+void eeprom_check(void)
+{
+	uint32_t magic;
+
+	EEPROM.get(EEPROM_MAGIC_OFFSET, magic);
+	if (magic == eeprom_magic)
+		return;
+
+	Serial.println("DEFAULTS!");
+	EEPROM.put(EEPROM_MAGIC_OFFSET, eeprom_magic);
+	EEPROM.put(EEPROM_NAME_OFFSET, eeprom_default_name);
+	EEPROM.put(EEPROM_WIFI_SSID_OFFSET, eeprom_default_wifi_ssid);
+	EEPROM.put(EEPROM_WIFI_PASS_OFFSET, eeprom_default_wifi_pass);
+	EEPROM.put(EEPROM_MAC_OFFSET, eeprom_default_mac);
+	EEPROM.put(EEPROM_IP_OFFSET, eeprom_default_ip);
+	EEPROM.put(EEPROM_MQTTIP_OFFSET, eeprom_default_mqttip);
+	EEPROM.put(EEPROM_FILTER_OFFSET, eeprom_default_filter);
+	EEPROM.put(EEPROM_THRESHOLD_OFFSET, eeprom_default_threshold);
+	EEPROM.put(EEPROM_PIN_FLAVOURS_OFFSET, eeprom_default_pin_flavours);
+	EEPROM.commit();
+}
+
+void payload_mac_to_eeprom(int offset, int size, byte *payload, int length)
+{
+	char *pos = (char *) payload;
+	uint8_t i;
+
+	for (i = 0; i < 6; i++) {
+		EEPROM.write(offset + i, strtol(pos, &pos, 16));
+		pos++;
+		if (pos >= (char *) payload + length)
+			return;
+	}
+	EEPROM.commit();
+}
+
+void str_to_eeprom(int offset, int size, byte *payload, int length)
+{
+	char *pos = (char *) payload;
+	uint8_t i;
+
+	for (i = 0; i < size; i++) {
+		if (i + 1 == size || i >= length)
+			EEPROM.write(offset + i, 0);
+		else
+			EEPROM.write(offset + i, *pos++);
+	}
+	EEPROM.commit();
+}
+
+bool check_pin_flavours_to_eeprom(char *topic, byte *payload,
+				  unsigned int length)
+{
+	struct pin *pin;
+	uint8_t i;
+
+	for_each_pin(pin, i) {
+		if (strcmp(topic, pin_config_topic(pin)))
+			continue;
+		if (!strcmp((const char *) payload,
+			    pin_flavour_subtopic[PIN_FLAVOUR_DISABLED]))
+			pin_flavours.pin_flavours[i] = PIN_FLAVOUR_DISABLED;
+		else if (!strcmp((const char *) payload,
+			    pin_flavour_subtopic[PIN_FLAVOUR_DIGITAL_INPUT]))
+			pin_flavours.pin_flavours[i] = PIN_FLAVOUR_DIGITAL_INPUT;
+		else if (!strcmp((const char *) payload,
+				 pin_flavour_subtopic[PIN_FLAVOUR_ANALOG_INPUT]))
+			pin_flavours.pin_flavours[i] = PIN_FLAVOUR_ANALOG_INPUT;
+		else if (!strcmp((const char *) payload,
+				 pin_flavour_subtopic[PIN_FLAVOUR_DIGITAL_OUTPUT]))
+			pin_flavours.pin_flavours[i] = PIN_FLAVOUR_DIGITAL_OUTPUT;
+		else if (!strcmp((const char *) payload,
+				 pin_flavour_subtopic[PIN_FLAVOUR_PWM_OUTPUT]))
+			pin_flavours.pin_flavours[i] = PIN_FLAVOUR_PWM_OUTPUT;
+		else
+			continue;
+		EEPROM.put(EEPROM_PIN_FLAVOURS_OFFSET, pin_flavours);
+		EEPROM.commit();
+		return true;
+	}
+	return false;
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+	payload[length] = '\0';
+
+	if (!strcmp(topic, config_topic("name"))) {
+		str_to_eeprom(EEPROM_NAME_OFFSET, EEPROM_NAME_SIZE,
+			      payload, length);
+	} else if (!strcmp(topic, config_topic("ssid"))) {
+		str_to_eeprom(EEPROM_WIFI_SSID_OFFSET, EEPROM_WIFI_SSID_SIZE,
+			      payload, length);
+	} else if (!strcmp(topic, config_topic("pass"))) {
+		str_to_eeprom(EEPROM_WIFI_PASS_OFFSET, EEPROM_WIFI_PASS_SIZE,
+			      payload, length);
+	} else if (!strcmp(topic, config_topic("mac"))) {
+		payload_mac_to_eeprom(EEPROM_MAC_OFFSET, EEPROM_MAC_SIZE,
+				      payload, length);
+	} else if (!strcmp(topic, config_topic("ip"))) {
+		IPAddress ip;
+
+		if (ip.fromString((const char *) payload)) {
+			EEPROM.put(EEPROM_IP_OFFSET, ip);
+			EEPROM.commit();
+		}
+	} else if (!strcmp(topic, config_topic("mqttip"))) {
+		IPAddress mqttip;
+
+		if (mqttip.fromString((const char *) payload)) {
+			EEPROM.put(EEPROM_MQTTIP_OFFSET, mqttip);
+			EEPROM.commit();
+		}
+	} else if (!strcmp(topic, config_topic("filter"))) {
+		uint32_t filter = strtol((const char *) payload, NULL, 10);
+
+		if (filter < EEPROM_FILTER_MAX) {
+			EEPROM.put(EEPROM_FILTER_OFFSET, filter);
+			EEPROM.commit();
+		}
+	} else if (!strcmp(topic, config_topic("threshold"))) {
+		uint32_t threshold = strtol((const char *) payload, NULL, 10);
+
+		if (threshold < EEPROM_THRESHOLD_MAX) {
+			EEPROM.put(EEPROM_THRESHOLD_OFFSET, (uint8_t) threshold);
+			EEPROM.commit();
+		}
+	} else if (check_pin_flavours_to_eeprom(topic, payload, length)) {
+	} else {
+		pins_msg_process(topic, (const char *) payload);
+	}
+}
+
+#define LCD_STATUS_WIDTH M5.Lcd.width()
+#define LCD_STATUS_HEIGHT 20
+#define LCD_STATUS_X 0
+#define LCD_STATUS_Y (M5.Lcd.height() - LCD_STATUS_HEIGHT)
+
+void print_status(String status)
+{
+	M5.Lcd.fillRect(LCD_STATUS_X, LCD_STATUS_Y,
+			LCD_STATUS_WIDTH, LCD_STATUS_HEIGHT, BLUE);
+	M5.Lcd.setTextColor(WHITE);
+	M5.Lcd.setTextSize(2);
+	M5.Lcd.setCursor(LCD_STATUS_X, LCD_STATUS_Y);
+	M5.Lcd.print(status);
+	Serial.println(status);
+}
+
+void setup(void)
+{
+	char wifi_ssid[WIFI_SSID_LEN];
+	char wifi_pass[WIFI_PASS_LEN];
+	byte mac[MAC_LEN];
+	IPAddress mqttip;
+	char macstr[13];
+	IPAddress ip;
+
+	Serial.println("MQTTIO");
+
+	M5.begin();
+	M5.Lcd.fillScreen(BLACK);
+	print_status("INIT");
+
+	EEPROM.begin(EEPROM_SIZE);
+	eeprom_check();
+
+	EEPROM.get(EEPROM_NAME_OFFSET, name);
+	Serial.print("NAME:");
+	Serial.println(name);
+	
+	EEPROM.get(EEPROM_WIFI_SSID_OFFSET, wifi_ssid);
+	Serial.print("SSID:");
+	Serial.println(wifi_ssid);
+	
+	EEPROM.get(EEPROM_WIFI_PASS_OFFSET, wifi_pass);
+	Serial.print("PASS:");
+	Serial.println(wifi_pass);
+
+	EEPROM.get(EEPROM_MAC_OFFSET, mac);
+	mac[0] &= 0xfe; /* Clear multicast bit. */
+	mac[0] |= 0x02; /* Set local assignment bit. */
+
+	sprintf(macstr, "%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+	Serial.print("MAC:");
+	Serial.println(macstr);
+
+	EEPROM.get(EEPROM_IP_OFFSET, ip);
+	Serial.print("IP:");
+	print_ip(ip);
+
+	EEPROM.get(EEPROM_MQTTIP_OFFSET, mqttip);
+	Serial.print("MQTTIP:");
+	print_ip(mqttip);
+
+	EEPROM.get(EEPROM_FILTER_OFFSET, input_filter);
+	Serial.print("FILTER:");
+	Serial.println(input_filter);
+	
+	EEPROM.get(EEPROM_THRESHOLD_OFFSET, input_threshold);
+	Serial.print("THRESHOLD:");
+	Serial.println(input_threshold);
+
+	EEPROM.get(EEPROM_PIN_FLAVOURS_OFFSET, pin_flavours);
+	Serial.println("PIN FLAVOURS:");
+	load_print_pin_flavours();
+
+	WiFi.mode(WIFI_STA);
+	WiFi.begin(wifi_ssid, wifi_pass);
+
+	pins_init();
+
+	client.setServer(mqttip, 1883);
+	client.setCallback(callback);
+	M5.Lcd.setCursor(10, 10);
+	print_status("INIT DONE");
+}
+
+#define MQTT_RETRY_TIMEOUT 5000
+
+static bool wifi_connected;
+static bool mqtt_connected;
+static unsigned long mqtt_last_attempt;
+
+void(* reset) (void) = 0;
+
+void loop(void)
+{
+	unsigned long now = millis();
+
+	M5.update();
+	if (M5.BtnC.wasPressed()) {
+		print_status("RESET");
+		reset();
+	}
+
+	if (!mqtt_connected && WiFi.status() == WL_CONNECTED) {
+		print_status("WIFI CONNECTED");
+		wifi_connected = true;
+	} else if (mqtt_connected && WiFi.status() != WL_CONNECTED) {
+		print_status("WIFI DISCONNECTED");
+		wifi_connected = false;
+	}
+
+	if (!client.connected() && wifi_connected) {
+		if (mqtt_connected) {
+			mqtt_last_attempt = 0;
+			print_status("MQTT DISCONNECTED");
+			mqtt_connected = false;
+		}
+		if (!mqtt_last_attempt ||
+		    mqtt_last_attempt + MQTT_RETRY_TIMEOUT < now ||
+		    mqtt_last_attempt > now) {
+			mqtt_last_attempt = now;
+
+			if (client.connect(name)) {
+				print_status("MQTT CONNECTED");
+				mqtt_connected = true;
+				input_pins_update_state();
+				input_pins_publish(false);
+				client.subscribe(config_topic("name"));
+				client.subscribe(config_topic("ssid"));
+				client.subscribe(config_topic("pass"));
+				client.subscribe(config_topic("mac"));
+				client.subscribe(config_topic("ip"));
+				client.subscribe(config_topic("mqttip"));
+				client.subscribe(config_topic("filter"));
+				client.subscribe(config_topic("threshold"));
+				pins_subscribe();
+			}
+		}
+	} else {
+		input_pins_update_state();
+		input_pins_publish(true);
+		client.loop();
+	}
+}
